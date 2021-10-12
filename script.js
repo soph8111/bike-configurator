5a0d2566a7759ed9ff24d8c3d263fde641246cee
Caroline <caro713f@stud.kea.dk>
2021-10-12 12:50:45 +0200

    kommentar på kode

--- script.js
+++ script.js
@@ -15,29 +15,38 @@ async function start() {
 }
 
 function chooseOptions(){
+  // Konstanter for ting der kan vælges farve på (stellet, bagjhul/forhjul)
   const frame = document.querySelector("#stel path");
   const frontWheel = document.querySelector("#forhjul path");
   const rearWheel = document.querySelector("#baghjul path");
 
+  // Vælg farve til stellet
   document.querySelectorAll(".bikecolor").forEach(color => {
+    // Klik på farve
     color.addEventListener("click", event => {
+      //Sæt bikeColor til at være det klikkede elements farve
       let bikeColor = event.target.style.backgroundColor;
       setBikeColor(frame, bikeColor);
     });
   })
 
+  // Vælg farve til hjul
   document.querySelectorAll(".wheelcolor").forEach(color => {
+    // Klik på hjul
     color.addEventListener("click", event => {
       let wheelColor = event.target.style.backgroundColor;
+      //Sæt wheelColor til at være det klikkede elements farve
       setWheelColor(frontWheel, rearWheel, wheelColor);
     });
   })
 }
 
+// Farver stellet efter hvilken farve der er klikket på
 function setBikeColor(frame, bikeColor){
   frame.style.fill = bikeColor;
 }
 
+// Farver hjulene efter hvilken farve der er klikket på 
 function setWheelColor(frontWheel, rearWheel, wheelColor){
   frontWheel.style.fill = wheelColor;
   rearWheel.style.fill = wheelColor;
