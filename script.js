f1a408bb819779b35a3a8684eb27fd69fbe74d5d
Caroline <caro713f@stud.kea.dk>
2021-10-12 11:31:54 +0200

    first attempt on colorselector

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ async function start() {
   let response = await fetch("cykelconfiguator.svg");
   let mySvg = await response.text();
   document.querySelector("section").innerHTML = mySvg;
+  chooseColor();
 }
+
+function chooseColor(){
+  const frontWheel = document.querySelector("#forhjul");
+  const rearWheel = document.querySelector("#baghjul");
+  const frame = document.querySelector("#stel");
+  const saddle1 = document.querySelector("#saddel_et");
+  const saddle2 = document.querySelector("#saddel_to");
+  const handle1 = document.querySelector("#styr_et");
+  const handle2 = document.querySelector("#styr_to");
+
+  document.querySelectorAll(".bikecolor").forEach(color => {
+    let bikeColor = color.style.backgroundColor;
+    color.addEventListener("click", setBikeColor(color, bikeColor));
+  } )
+}
+
+function setBikeColor(color, bikeColor){
+  color.style.backgroundColor = bikeColor;
+}
+
+
