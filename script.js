3d5f49beac1e14e3dda46fcf1d29c2df24e84b0e
Caroline <caro713f@stud.kea.dk>
2021-10-13 11:14:23 +0200

    animation on selected items

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ const addOnFeatures = {
   kickstand: false
 };
 
-const saddelFeatures = {
+const saddleFeatures = {
   saddle1: true,
   saddle2: false
-}
+};
+
+const handleFeatures = {
+  handle1: true,
+  handle2: false
+};
+
 
 async function start() {
   let response = await fetch("cykelconfiguator.svg");
@@ -48,6 +54,23 @@ function chooseOptions(){
     });
   });
 
+  // Vælg saddel
+  document.querySelectorAll(".saddle_option").forEach(option => option.addEventListener("click", toggleSaddle));
+
+  // Vælg håndtag
+  document.querySelectorAll(".handle_option").forEach(option => option.addEventListener("click", toggelHandle));
+    
+
+   //Vælg farve på håndtag og sadel
+   document.querySelectorAll(".leathercolor").forEach(color => {
+    // Klik på læder
+    color.addEventListener("click", event => {
+      let leatherColor = event.target.style.backgroundColor;
+      //Sæt wheelColor til at være det klikkede elements farve
+      setLeatherColor(saddle1, saddle2, handle1, handle2, leatherColor);
+    });
+});
+
   // HJUL
   // Vælg farve til hjul
   document.querySelectorAll(".wheelcolor").forEach(color => {
@@ -59,18 +82,108 @@ function chooseOptions(){
     });
   });
 
-    //Vælg farve på håndtag og sadel
-    document.querySelectorAll(".leathercolor").forEach(color => {
-      // Klik på læder
-      color.addEventListener("click", event => {
-        let leatherColor = event.target.style.backgroundColor;
-        //Sæt wheelColor til at være det klikkede elements farve
-        setLeatherColor(saddle1, saddle2, handle1, handle2, leatherColor);
-      });
-  });
-
   // ADD ONS
   document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleAddOns));
+}
+
+function toggleSaddle(event){
+  const target = event.currentTarget;
+  const saddle = target.dataset.feature;
+
+   // Toggle saddle true/false
+   if (saddle) {
+    saddleFeatures[saddle] = !saddleFeatures[saddle];
+  }
+
+  // Hvis saddel er on
+  if (saddleFeatures[saddle]){
+    console.log(`${saddle} is on`);
+
+    // Marker valgte saddel
+    target.classList.add("chosen");
+
+    //Fjern anden valgt saddel
+
+    //Fjerne hide fra product view - så addOn vises
+    document.querySelector(`[data-feature='${saddle}']`).classList.remove("hide");
+
+    // Sæt hide på anden saddel i productview
+
+    // Lav addOn element og tilføj til selected items liste
+    const saddleElement = createAddOnElement(saddle);
+    document.querySelector("#selected ul").append(saddleElement);
+
+    // Animation på tilføj til selected items
+    animateAdd(target, saddleElement);
+  }
+
+  // Hvis saddel er off
+  else {
+    console.log(`${saddle} is off`);
+
+    //Fjerne markering af valgte addON
+    target.classList.remove("chosen");
+
+    //Sætte hide på product view - skjule addOn
+    document.querySelector(`[data-feature='${saddle}']`).classList.add("hide");
+
+    // Find addOn element og fjern fra selected items liste
+    const saddleElement = document.querySelector(`#selected [data-feature='${saddle}']`);
+
+    // Animation på fjern fra selected items
+    animateRemove(target, saddleElement);
+  }
+}
+
+function toggelHandle(event){
+  const target = event.currentTarget;
+  const handle = target.dataset.feature;
+
+   // Toggle saddle true/false
+   if (handle) {
+    handleFeatures[handle] = !handleFeatures[handle];
+  }
+
+  // Hvis saddel er on
+  if (handleFeatures[handle]){
+    console.log(`${handle} is on`);
+
+    // Marker valgte saddel
+    target.classList.add("chosen");
+
+    //Fjern anden valgt saddel
+
+    //Fjerne hide fra product view - så addOn vises
+    document.querySelector(`[data-feature='${handle}']`).classList.remove("hide");
+
+    // Sæt hide på anden saddel i productview
+
+    // Lav addOn element og tilføj til selected items liste
+    const handleElement = createAddOnElement(handle);
+    document.querySelector("#selected ul").append(handleElement);
+
+    // Animation på tilføj til selected items
+    animateAdd(target, handleElement);
+  }
+
+  // Hvis saddel er off
+  else {
+    console.log(`${handle} is off`);
+
+    //Fjerne markering af valgte addON
+    target.classList.remove("chosen");
+
+    //Sætte hide på product view - skjule addOn
+    document.querySelector(`[data-feature='${handle}']`).classList.add("hide");
+
+    // Find addOn element og fjern fra selected items liste
+    const handleElement = document.querySelector(`#selected [data-feature='${handle}']`);
+
+    // Animation på fjern fra selected items
+    animateRemove(target, handleElement);
+    
+  }
+}
 
 
 function toggleAddOns(event){
@@ -95,6 +208,9 @@ function toggleAddOns(event){
     // Lav addOn element og tilføj til selected items liste
     const addOnElement = createAddOnElement(addOn);
     document.querySelector("#selected ul").append(addOnElement);
+
+    // Animation på tilføj til selected items
+    animateAdd(target, addOnElement);
   }
 
   // Hvis addOn er off
@@ -108,10 +224,13 @@ function toggleAddOns(event){
     document.querySelector(`[data-feature='${addOn}']`).classList.add("hide");
 
     // Find addOn element og fjern fra selected items liste
-    document.querySelector(`#selected [data-feature='${addOn}']`).remove();
+    const addOnElement = document.querySelector(`#selected [data-feature='${addOn}']`);
+
+    // Animation på fjern fra selected items
+    animateRemove(target, addOnElement);
   }
 }
-}
+
 // Farver stellet efter hvilken farve der er klikket på
 function setBikeColor(frame, bikeColor){
   frame.style.fill = bikeColor;
@@ -144,3 +263,52 @@ function createAddOnElement(addOn){
   return li;
 }
   
+// Animation
+
+function animateAdd(target, featureElement){
+  // create FLIP-animation to animate featureElement from img in target, to its intended position. Do it with normal animation or transition class!
+    const clickFrame = target.getBoundingClientRect();
+    const addedFrame = featureElement.getBoundingClientRect();
+
+    const deltaX = clickFrame.left - addedFrame.left;
+    const deltaY = clickFrame.top - addedFrame.top;
+    // const deltaWidth = clickFrame.width / addedFrame.width;
+    // const deltaHeight = clickFrame.height / addedFrame.height;
+
+    featureElement.animate(
+      [
+        { transformOrigin: "top left", transform: `translateX(${deltaX}px) translateY(${deltaY}px)` },
+        { transformOrigin: "top left", transfrom: "none" }
+      ],
+      {
+        duration: 1000,
+        easing: "ease-in-out"
+      }
+    );
+}
+
+function animateRemove(target, featureElement){
+  // - create FLIP-animation to animate featureElement to img in target
+  const clickFrame = target.getBoundingClientRect();
+  const addedFrame = featureElement.getBoundingClientRect();
+
+  const deltaX = clickFrame.left - addedFrame.left;
+  const deltaY = clickFrame.top - addedFrame.top;
+  console.log(deltaY);
+
+  const remove = featureElement.animate(
+    [
+      { transformOrigin: "top left", transfrom: "none" },
+      { transformOrigin: "top left", transform: `translateX(${deltaX}px) translateY(${deltaY}px)` }
+    ],
+    {
+      duration: 1000,
+      easing: "ease-in-out"
+    }
+  );
+
+  // - when animation is complete, remove featureElement from the DOM
+  remove.onfinish = () => {
+    featureElement.remove();
+  }
+}
