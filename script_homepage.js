c0102c4e8275d87284d45bad750f5495f42416eb
Sophie Spang <soph8111@stud.kea.dk>
2021-10-14 12:14:53 +0200

    index.html styling splash

--- script_homepage.js
+++ script_homepage.js
@@ -1,10 +1,14 @@
+"use strict";
+
+window.addEventListener("DOMContenLoaded", start());
+
 // Inspired by: https://codepen.io/GreenSock/pen/gOabMXv
 
 gsap
   .timeline({
     scrollTrigger: {
       trigger: ".frame",
-      start: "20vw 35%",
+      start: "top 35%",
       end: "bottom 85%",
       scrub: true,
       pin: true,
@@ -20,3 +24,18 @@ gsap
   .from(".bell", { y: innerHeight * 1.5 })
   .from(".basket", { y: innerHeight * 1.5 })
   .from(".kickstand", { y: innerHeight * 1.5 });
+
+function start() {
+  console.log("start");
+  const properties = {
+    duration: 800,
+    iterations: Infinity,
+    direction: "alternate",
+    easing: "ease-out",
+  };
+
+  const keyframes = [{ transform: `translate(0, 1vw)` }, { tranform: `transform: translate(0,0)` }];
+
+  const arrow = document.querySelector("#arrow");
+  const animation = arrow.animate(keyframes, properties);
+}
